3db89fefc57c410e6305f2ce19b3d8d31a263dd0
saurabhy990 <saurabhy990@gmail.com>
2024-01-12 21:28:41 +0530

    domproject

---
+++ index.js
@@ -0,0 +1,13 @@
+ let div = document.getElementById("root");
+ div.style.backgroundColor = "yellow";
+ div.style.height = "200px";
+ div.style.width = "300px";
+ div.style.margin = "30px";
+ div.style.padding = "10px";
+
+ let fnt = div.querySelector("h1") 
+ //let fnt = document.querySelector("h1")
+  fnt.style.fontSize = "18px"
+
+  fnt.style.fontWeight = "bold";
+  fnt.style.color = "blue";
